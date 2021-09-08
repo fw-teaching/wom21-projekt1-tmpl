41dbd8d18c9b0986e6e3ce729b750a86ea96e3bf
Fredrik Welander <76183002+welandfr@users.noreply.github.com>
2021-09-08 17:00:07 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Testa att detta funkar, ersätt sedan med egen kod
-console.log("Projekt 1")
-app.get('/', (req, res) => res.json("Projekt 1"))
+console.log("Projekt 1");
+app.get('/', (req, res) => res.json("Projekt 1"));
 
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
